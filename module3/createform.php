2dbef504bed1e5cad24d266093e515525cdf3f01
sjohn0731 <34875798+sjohn0731@users.noreply.github.com>
2025-07-07 00:45:47 -0700

    created form

--- module3/createform.php
+++ module3/createform.php
@@ -6,8 +6,15 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="" method="POST">
+       <input type="text" name="fullname" placeholder="Full Name; i.e John Smith"> 
+       <input type="email" name="email" placeholder="JohnSmith123@yahoo.com">
+       <input type="text" name="topic" placeholder="I.E., Life Finds a Way.">
+       <input type="textarea" name="message" placeholder="...Well, there it is.">
+       <input type="submit" value="Submit">
+    </form>
     <?php
-    
+
     
     ?>
 </body>
